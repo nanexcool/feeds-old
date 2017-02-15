5188fa77fab868e062173e3cb2df4ecb815335be
Mariano Conti <nanexcool@gmail.com>
2017-02-15 20:33:52 -0300

    Change the way feeds detects if there is no RPC connection

--- web3.js
+++ web3.js
@@ -1,7 +1,17 @@
 const Web3 = require('web3');
 
+const HOST = process.env.ETH_RPC_HOST || 'localhost';
+const PORT = process.env.ETH_RPC_PORT || 8545;
+const URL = process.env.ETH_RPC_URL || `http://${HOST}:${PORT}`;
+
 const web3 = new Web3(this.web3 ? this.web3.currentProvider : (
-  new Web3.providers.HttpProvider('http://localhost:8545')
+  new Web3.providers.HttpProvider(URL)
 ));
+
+if (!web3.isConnected()) {
+  const message = `Could not connect to Ethereum RPC server at ${URL}`;
+  console.log(message);
+  process.exit(1);
+}
 
 module.exports = web3;
